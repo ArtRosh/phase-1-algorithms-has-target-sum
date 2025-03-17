1f74cf732c12222046e733f4f305004ceb168782
ArtRosh <artem.rosh17@gmail.com>
2025-03-17 14:23:34 -0700

    lab

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = new Set();
+
+  for (const number of array) {
+    const difference = target - number;
+
+    if (seenNumbers.has(difference)) {
+      return true;
+    }
+
+    seenNumbers.add(number);
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  O(n) - we loop through the array once
+  
 */
 
 /* 
   Add your pseudocode here
+
+create an empty set to store seen numbers
+
+loop through each number in the array
+  calculate the difference between the target and the current number
+  if the difference is in the set, return true
+  add the current number to the set
+
+  if the loop without finding a pair, return false
 */
 
 /*
